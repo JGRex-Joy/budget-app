c7ad980f2fe07ba21e8e5533f87e3c8b4480cc51
JGRex-Joy <omurkulovamir2008@gmail.com>
2026-01-15 21:05:22 +0600

    created auth service

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_NAME: str = "Budget App Backend"
     DATABASE_URL: str = "postgresql://budget:budget58@db:5432/budgetdb"
+    SECRET_KEY: str = "bakaibankisonelove"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
     
