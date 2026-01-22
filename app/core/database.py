e68c2f0b49d5b9343ab92bd7d70c0cd409e5b150
JGRex-Joy <omurkulovamir2008@gmail.com>
2026-01-22 16:01:24 +0600

    preparing for deployment

--- app/core/database.py
+++ app/core/database.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL,
+                       pool_pre_ping=True,
+                       connect_args={"sslmode": "require"},)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
